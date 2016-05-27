60d6c4f4f46cfc34757fcccbb42afd7dce15ddfe
duanchuanfen <admin@app.local>
2016-05-27 09:43:04 +0800

    Initial Commit

---
+++ UItextFielddemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UItextFielddemo
+//
+//  Created by duanchuanfen on 16/5/27.
+//  Copyright © 2016年 duanchuanfen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
